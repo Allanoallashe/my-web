f59eb339c3b899dcecf23bb1519e6725528cc6d3
Allan Lenny <owinoallan77@gmail.com>
2023-01-19 12:07:43 +0300

    made the website responsive

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
 
-menu.addEventListener('click', function()
+menu.addEventListener('click', function(event)
   {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
